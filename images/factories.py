c45f68dedab04f73433e541f2e9036995aee63c3
Y3K <dboy.y3k@gmail.com>
2014-05-22 02:39:03 -0500

    Factories Fix

--- images/factories.py
+++ images/factories.py
@@ -5,6 +5,8 @@ import factory
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.webdesign.lorem_ipsum import paragraphs, words
 
+from commercia.products.models import Product
+
 from faker import Factory
 
 fake = Factory.create()
@@ -23,5 +25,7 @@ class ImageInstanceFactory(factory.django.DjangoModelFactory):
 
     image = factory.SubFactory(ImageFactory)
 
-    content_type = ContentType.objects.first() # Let's just ram it up
-    object_id = 1
+    content_type = ContentType.objects.get(app_label='products',
+        model='product')
+    object_id = factory.LazyAttribute(lambda o: randint(1,
+        Product.objects.count()))
