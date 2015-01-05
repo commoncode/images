1b227d35d33f8e618f999ccbaf83365609791af2
Y3K <dboy.y3k@gmail.com>
2015-01-04 19:17:37 -0600

    Update command

--- images/management/commands/create_images.py
+++ images/management/commands/create_images.py
@@ -1,18 +1,45 @@
+from random import randint
+
 from django.core.management.base import BaseCommand
 
-from ...factories import ImageFactory
+from images.factories import ImageFactory
 
 
 class Command(BaseCommand):
-    args = '[quantity]'
-    help = 'Create Images'
+    args = '<type> [quantity]'
+    help = (
+        'Image types: book | cosmetic | food | garment | software | vehicle'
+    )
 
     def handle(self, *args, **options):
         try:
-            quantity = args[0]
+            category = {
+                'book': 'business',
+                'cosmetic': 'people',
+                'food': 'food',
+                'garment': 'fashion',
+                'software': 'technics',
+                'vehicle': 'transport'
+            }.get(args[0].lower())
         except IndexError:
-            quantity = 50
+            return (
+                'Please declare a type of image, read help for available '
+                'options.'
+            )
+
+        try:
+            quantity = int(args[1])
+        except IndexError:
+            quantity = randint(20, 30)
+
+        print('Creating Images')
 
         for i in range(quantity):
-            image = ImageFactory()
-            print('Added Image: {}'.format(image.title))
+            url = 'http://lorempixel.com/{width}/{height}/{category}'.format(
+                width=randint(100, 500),
+                height=randint(100, 500),
+                category=category
+
+            )
+            ImageFactory(url=url)
+            print(url)
